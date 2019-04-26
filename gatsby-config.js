5af947506b3b8ed5e0b99e11b910aef477376664
Joshua Gagnon <josh.n.gagnon@gmail.com>
2019-04-26 17:21:56 +1000

    trying preload fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -139,6 +139,24 @@ module.exports = {
     },
       `gatsby-plugin-favicon`,
     "gatsby-plugin-offline",
+
+
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Roboto`,
+            variants: [`400`,`700`],
+          },
+          {
+            family: `Material Icons`,
+          }
+        ],
+      },
+    }
+
+
     /*{
       resolve: "gatsby-plugin-feed",
       options: {
